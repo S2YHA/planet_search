341379545919d9b3c8c50eed16d29ab2bcb51cb2
S2YHA <pawel.kaminski10@gmail.com>
2024-06-01 11:47:43 +0200

    basic datatable component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
-<script setup>
-import Button from './components/atoms/Button.vue'
-import Input from './components/atoms/Input.vue'
-</script>
-
 <template>
-  <header></header>
-  <Button />
-  <Input />
-  <main></main>
+  <DataTable :headers="headers" :data="planetsData" />
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import DataTable from './components/molecules/DataTable.vue'
+
+const planetsData = ref([])
+const headers = ['Name', 'Population', 'Rotation Period', 'Climate', 'Gravity', 'Created', 'Url']
+
+onMounted(() => {
+  fetch('https://swapi.dev/api/planets')
+    .then(function (response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' + response.status)
+        return
+      }
+      response.json().then(function (data) {
+        planetsData.value = data.results.map((planet) => {
+          return {
+            name: planet.name,
+            population: planet.population,
+            rotationPeriod: planet.rotation_period,
+            climate: planet.climate,
+            gravity: planet.gravity,
+            created: planet.created,
+            url: planet.url
+          }
+        })
+      })
+    })
+    .catch(function (err) {
+      console.log('Fetch Error', err)
+    })
+})
+</script>
 
 <style scoped></style>
---
+++ src/components/molecules/DataTable.vue
@@ -0,0 +1,27 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th v-for="header in headers">{{ header }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="row in data" :key="JSON.parse(JSON.stringify(row))">
+        <td v-for="(value, _key) in row">{{ value }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup>
+defineProps({
+  headers: {
+    type: Array,
+    required: true
+  },
+  data: {
+    type: Array,
+    required: true
+  }
+})
+</script>
